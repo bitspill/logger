7bb3a84c2e07337811920a8fdb7bb318b54a5155
Jeremiah Buddenhagen <git@bitspill.net>
2018-12-10 14:24:17 -0800

    force colors

--- standard-output.go
+++ standard-output.go
@@ -3,7 +3,7 @@ package logger
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/azer/is-terminal"
+	// "github.com/azer/is-terminal"
 	"os"
 	"strings"
 	"time"
@@ -11,7 +11,7 @@ import (
 
 func NewStandardOutput(file *os.File) OutputWriter {
 	var writer = StandardWriter{
-		ColorsEnabled: isterminal.IsTerminal(int(file.Fd())),
+		ColorsEnabled: true, // isterminal.IsTerminal(int(file.Fd())),
 		Target:        file,
 	}
 
